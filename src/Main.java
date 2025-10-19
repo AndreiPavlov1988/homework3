28e9314a73ff75d9073192af771469e5b004e732
AndreiPavlov1988 <ai_pavlov@mail.ru>
2025-10-19 18:33:14 +0300

    task 3

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,25 @@
 //task 1
 public class Main {
     public static void main(String[] args) {
-int age = 18;
-if (age >= 18) {
-System.out.println("Если возраст человека равен " + age + " то он совершеннолетний");}
-else {System.out.println("Если возраст человека равен" + age + "то он не достиг совершеннолетия, нужно немного подождать");
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + " то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен" + age + "то он не достиг совершеннолетия, нужно немного подождать");
+        }
+//task 2
+        int temperature = 4;
+        if (temperature < 5) {
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
+        } else {
+            System.out.println("На улице" + temperature + " градусов, можно идти без шапки");
+        }
+//task3
+        int speed = 80;
+        if (speed > 60) {
+            System.out.println("Если скорость " + speed + ", то придется заплатить штраф");
+        } else {
+            System.out.println("Если скорость " + speed + ", то можно ездить спокойно");
         }
     }
 }
